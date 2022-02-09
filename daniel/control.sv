0f37d122e2cc7ca56bb48b263c60e88dacc69f53
danlei275 <85211533+danlei275@users.noreply.github.com>
2022-02-08 19:36:28 -0800

    Update control.sv

--- daniel/control.sv
+++ daniel/control.sv
@@ -1,16 +1,19 @@
-module control(instr, branch, zeroFlag, Reg2Loc, ALUSrc, MemToReg, RegWrite, MemWrite, BrTaken, UncondBr, ALUOp, setFlag);
+   
+module control(instr, zeroFlag, Reg2Loc, ALUSrc, MemToReg, 
+					RegWrite, MemWrite, BrTaken, UncondBr, ALUOp, setFlag, 
+					immSize, shiftSel, negative, overflow);
 	input logic [31:0] instr;
-	input logic branch, zeroFlag;
+	input logic zeroFlag, negative, overflow;
 	output logic Reg2Loc, ALUSrc,
 		MemToReg, RegWrite,
 		MemWrite, BrTaken,
-		UncondBr;
+		UncondBr, immSize, shiftSel;
 	output logic [2:0] ALUOp;
 	output logic setFlag;
 	
 	always_comb begin
 		// ---- R type ----
-		// ADDI
+		// ADDI - had error in aluOP
 		// ADDI Rd, Rn, Imm12: Reg[Rd] = Reg[Rn] + ZeroExtend(Imm12). 
 		if (instr[31:22] == 10'h244) begin
 			Reg2Loc = 1'bx;
@@ -20,12 +23,14 @@ module control(instr, branch, zeroFlag, Reg2Loc, ALUSrc, MemToReg, RegWrite, Mem
 			MemWrite = 1'b0;
 			BrTaken = 1'b0;
 			UncondBr = 1'bx;
-			ALUOp = 3'b100;
+			ALUOp = 3'b010;
+			immSize = 1'b1;
+			shiftSel = 1'b0;
 			// ----
-			setFlag = 0;
+			setFlag = 1'b0;
 		end
 
-		// ADDS
+		// ADDS - had error in aluOP
 		// ADDS Rd, Rn, Rm: Reg[Rd] = Reg[Rn] + Reg[Rm]. Set flags.
 		else if (instr[31:21] == 11'h558) begin
 			Reg2Loc = 1'b1;
@@ -35,12 +40,14 @@ module control(instr, branch, zeroFlag, Reg2Loc, ALUSrc, MemToReg, RegWrite, Mem
 			MemWrite = 1'b0;
 			BrTaken = 1'b0;
 			UncondBr = 1'bx;
-			ALUOp = 3'b100;
+			ALUOp = 3'b010;
+			immSize = 1'bX;
+			shiftSel = 1'b0;
 			// ----
-			setFlag = 1;
+			setFlag = 1'b1;
 		end
 
-		// AND
+		// AND - had error in aluOP
 		// AND Rd, Rn, Rm: Reg[Rd] = Reg[Rn] & Reg[Rm]. 
 		else if (instr[31:21] == 11'h450) begin
 			Reg2Loc = 1'b1;
@@ -50,9 +57,11 @@ module control(instr, branch, zeroFlag, Reg2Loc, ALUSrc, MemToReg, RegWrite, Mem
 			MemWrite = 1'b0;
 			BrTaken = 1'b0;
 			UncondBr = 1'bx;
-			ALUOp = 3'b010;
+			ALUOp = 3'b100;
+			immSize = 1'bX;
+			shiftSel = 1'b0;
 			// ----
-			setFlag = 0;
+			setFlag = 1'b0;
 		end
 		
 		// LDUR
@@ -66,12 +75,15 @@ module control(instr, branch, zeroFlag, Reg2Loc, ALUSrc, MemToReg, RegWrite, Mem
 			BrTaken = 1'b0;
 			UncondBr = 1'bx;
 			ALUOp = 3'b010;
+			immSize = 1'b0;
+			shiftSel = 1'b0;
 			// ----
-			setFlag = 0;
+			setFlag = 1'b0;
 		end
 		
-		// ORR
-		else if (instr[31:21] == 11'h550) begin
+		//  
+		// EOR Rd, Rn, Rm: Reg[Rd] = Reg[Rn] ^ Reg[Rm].
+		else if (instr[31:21] == 11'h650) begin
 			Reg2Loc = 1'b1;
 			ALUSrc = 1'b0;
 			MemToReg = 1'b0;
@@ -79,9 +91,11 @@ module control(instr, branch, zeroFlag, Reg2Loc, ALUSrc, MemToReg, RegWrite, Mem
 			MemWrite = 1'b0;
 			BrTaken = 1'b0;
 			UncondBr = 1'bx;
-			ALUOp = 3'b101;
+			ALUOp = 3'b110;
+			immSize = 1'bX;
+			shiftSel = 1'b0;
 			// ----
-			setFlag = 0;
+			setFlag = 1'b0;
 		end
 
 		// LSR
@@ -95,8 +109,10 @@ module control(instr, branch, zeroFlag, Reg2Loc, ALUSrc, MemToReg, RegWrite, Mem
 			BrTaken = 1'b0;
 			UncondBr = 1'bx;
 			ALUOp = 3'bxxx;
+			immSize = 1'bX;
+			shiftSel = 1'b1;
 			// ----
-			setFlag = 0;
+			setFlag = 1'b0;
 		end
 
 		// STUR 
@@ -104,14 +120,16 @@ module control(instr, branch, zeroFlag, Reg2Loc, ALUSrc, MemToReg, RegWrite, Mem
 		else if (instr[31:21] == 11'h7C0) begin
 			Reg2Loc = 1'b0;
 			ALUSrc = 1'b1;
-			MemToReg = 1'bx;
+			MemToReg = 1'bX;
 			RegWrite = 1'b0;
 			MemWrite = 1'b1;
 			BrTaken = 1'b0;
 			UncondBr = 1'bx;
 			ALUOp = 3'b010;
+			immSize = 1'b0;
+			shiftSel = 1'b0;
 			// ----
-			setFlag = 0;
+			setFlag = 1'b0;
 		end
 
 		// SUBS
@@ -125,8 +143,10 @@ module control(instr, branch, zeroFlag, Reg2Loc, ALUSrc, MemToReg, RegWrite, Mem
 			BrTaken = 1'b0;
 			UncondBr = 1'bx;
 			ALUOp = 3'b011;
+			immSize = 1'bX;
+			shiftSel = 1'b0;
 			// -----
-			setFlag = 1;
+			setFlag = 1'b1;
 		end
 		
 		
@@ -142,28 +162,32 @@ module control(instr, branch, zeroFlag, Reg2Loc, ALUSrc, MemToReg, RegWrite, Mem
 			BrTaken = 1'b1;
 			UncondBr = 1'b1;
 			ALUOp = 3'bx;
-            // ----
-            setFlag = 0;
+			immSize = 1'bX;
+			shiftSel = 1'bX;
+         // ----
+         setFlag = 1'b0;
 		end
 
-		// B.LT
-        // B.LT Imm19: If (flags.negative != flags.overflow) PC = PC + SignExtend(Imm19<<2). 
-		else if ((instr[31:24] == 8'h54) && (instr[4:0] == 5'h0B)) begin
+		// B.LT insert extra condition with flags
+      // B.LT Imm19: If (flags.negative != flags.overflow) PC = PC + SignExtend(Imm19<<2). 
+		else if ((instr[31:24] == 8'h54) && (instr[4:0] == 5'h0B) && (negative != overflow)) begin
 			Reg2Loc = 1'bx;
 			ALUSrc = 1'bx;
 			MemToReg = 1'bx;
 			RegWrite = 1'b0;
 			MemWrite = 1'b0;
-			BrTaken = branch;
+			BrTaken = 1'b1; 
 			UncondBr = 1'b0;
 			ALUOp = 3'bx;
-            // ----
-            setFlag = 0;
+			immSize = 1'bX;
+			shiftSel = 1'bX;
+         // ----
+         setFlag = 1'b0;
 		end
 		
 		// CBZ
         // CBZ Rd, Imm19: If (Reg[Rd] == 0) PC = PC + SignExtend(Imm19<<2). 
-		else begin // (instr[31:26] == 6'hB4) begin:
+		else if (instr[31:24] == 8'hB4) begin
 			Reg2Loc = 1'b0;
 			ALUSrc = 1'b0;
 			MemToReg = 1'bx;
@@ -172,21 +196,39 @@ module control(instr, branch, zeroFlag, Reg2Loc, ALUSrc, MemToReg, RegWrite, Mem
 			BrTaken = zeroFlag; // zero flag
 			UncondBr = 1'b0;
 			ALUOp = 3'b000;
-            // ----
-            setFlag = 0;
+			immSize = 1'bX;
+			shiftSel = 1'bX;
+         // ----
+         setFlag = 1'b0;
+		end
+		
+		else begin //always_comb is always going here
+			Reg2Loc = 1'bX;
+			ALUSrc = 1'bX;
+			MemToReg = 1'bx;
+			RegWrite = 1'b0;
+			MemWrite = 1'b0;
+			BrTaken = 1'b0; // zero flag
+			UncondBr = 1'bX;
+			ALUOp = 3'bXXX;
+			immSize = 1'bX;
+			shiftSel = 1'bX;
+         // ----
+         setFlag = 1'b0;
 		end
 	end
 	
 endmodule 
 
-`timescale 1ns/1ns
+`timescale 1ps/1ps
 module control_testbench ();
     logic [31:0] instr;
-	logic branch, zeroFlag;
+	logic zeroFlag;
     logic Reg2Loc, ALUSrc,
 		MemToReg, RegWrite,
 		MemWrite, BrTaken,
-		UncondBr;
+		UncondBr, immSize, shiftSel,
+		negative, overflow;
 	logic [2:0] ALUOp;
 	logic setFlag;
 
@@ -195,25 +237,25 @@ module control_testbench ();
     initial begin
         // ADDI
 		// ADDI Rd, Rn, Imm12: Reg[Rd] = Reg[Rn] + ZeroExtend(Imm12)
-        instr = 32'b10010001000000000000001111100000; branch = 1'bx; zeroFlag = 1'bx; #10; // ADDI X0, X31, #0     // X0 = 0
+        instr = 32'b10010001000000000000001111100000;  zeroFlag = 1'bX; #10; // ADDI X0, X31, #0     // X0 = 0
         assert ((ALUSrc == 1'b1) && (MemToReg == 1'b0) && (RegWrite == 1'b1)
-			&& (MemWrite == 1'b0) && (BrTaken == 1'b0) && (ALUOp == 3'b100) && (setFlag == 0));
+			&& (MemWrite == 1'b0) && (BrTaken == 1'b0) && (ALUOp == 3'b010) && (setFlag == 0));
 
-        instr = 32'b10010001000000000000010000000001; branch = 1'bx; zeroFlag = 1'bx; #10; // ADDI X1, X0, #1      // X1 = 1
+        instr = 32'b10010001000000000000010000000001;  zeroFlag = 1'bx; #10; // ADDI X1, X0, #1      // X1 = 1
         assert ((ALUSrc == 1'b1) && (MemToReg == 1'b0) && (RegWrite == 1'b1)
-			&& (MemWrite == 1'b0) && (BrTaken == 1'b0) && (ALUOp == 3'b100) && (setFlag == 0));
+			&& (MemWrite == 1'b0) && (BrTaken == 1'b0) && (ALUOp == 3'b010) && (setFlag == 0));
 
-        instr = 32'b10010001000000000000010000100010; branch = 1'bx; zeroFlag = 1'bx; #10; // ADDI X2, X1, #1      // X2 = 2
+        instr = 32'b10010001000000000000010000100010; zeroFlag = 1'bx; #10; // ADDI X2, X1, #1      // X2 = 2
         assert ((ALUSrc == 1'b1) && (MemToReg == 1'b0) && (RegWrite == 1'b1)
-			&& (MemWrite == 1'b0) && (BrTaken == 1'b0) && (ALUOp == 3'b100) && (setFlag == 0));
+			&& (MemWrite == 1'b0) && (BrTaken == 1'b0) && (ALUOp == 3'b010) && (setFlag == 0));
 
-        instr = 32'b10010001000000000000100000100011; branch = 1'bx; zeroFlag = 1'bx; #10; // ADDI X3, X1, #2      // X3 = 3
+        instr = 32'b10010001000000000000100000100011; zeroFlag = 1'bx; #10; // ADDI X3, X1, #2      // X3 = 3
         assert ((ALUSrc == 1'b1) && (MemToReg == 1'b0) && (RegWrite == 1'b1)
-			&& (MemWrite == 1'b0) && (BrTaken == 1'b0) && (ALUOp == 3'b100) && (setFlag == 0));
+			&& (MemWrite == 1'b0) && (BrTaken == 1'b0) && (ALUOp == 3'b010) && (setFlag == 0));
 
-        instr = 32'b10010001000000000001000000000100; branch = 1'bx; zeroFlag = 1'bx; #10; // ADDI X4, X0, #4      // X4 = 4
+        instr = 32'b10010001000000000001000000000100; zeroFlag = 1'bx; #10; // ADDI X4, X0, #4      // X4 = 4
         assert ((ALUSrc == 1'b1) && (MemToReg == 1'b0) && (RegWrite == 1'b1)
-			&& (MemWrite == 1'b0) && (BrTaken == 1'b0) && (ALUOp == 3'b100) && (setFlag == 0));
+			&& (MemWrite == 1'b0) && (BrTaken == 1'b0) && (ALUOp == 3'b010) && (setFlag == 0));
 
     end
 
