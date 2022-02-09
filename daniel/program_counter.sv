a4e75905f61a21d28003bbd9b92e0b7a48eae153
danlei275 <85211533+danlei275@users.noreply.github.com>
2022-02-08 19:34:33 -0800

    Update program_counter.sv

--- daniel/program_counter.sv
+++ daniel/program_counter.sv
@@ -1,8 +1,8 @@
-module program_counter ( instruction, UncondBr, BrTaken, clk);
+module program_counter ( instruction, UncondBr, BrTaken, clk, reset);
 //	input logic [63:0] instr_addr; // address of instruction
 	output logic [31:0] instruction;
 	input logic UncondBr, BrTaken; // control inputs
-	input logic clk;
+	input logic clk, reset;
 	
 	logic [63:0] instr_addr;
 	
@@ -13,28 +13,63 @@ module program_counter ( instruction, UncondBr, BrTaken, clk);
 	
 	// sign extend condAddr19 and BrAddr26 then shift left by 2
 	logic [63:0] SE_condAddr19, SE_BrAddr26;
-	assign SE_BrAddr26 = {{38{instruction[25]}}, instruction[25:0]} << 2;
-	assign SE_condAddr19 = {{45{instruction[23]}}, instruction[23:5]} << 2;
+	// need to update this value
+	
+	// shift SE BrAddr26 and SE condAddr19
+	shifter shiftBrAddr(.value({{38{instruction[25]}}, instruction[25:0]}),.direction(1'b0),.distance(6'd2),.result(SE_BrAddr26));
+	shifter shiftcondAddr(.value({{45{instruction[23]}}, instruction[23:5]}),.direction(1'b0),.distance(6'd2),.result(SE_condAddr19));
 	
 	// 2:1 Mux to choose condAddr or BrAddr based on UncondBr control signal
 	logic [63:0] addr;
-	mux2_1 UncondBr_mux (.s0(UncondrBr), .a(SE_CondAddr19), .b(SE_CondBrAddr26), .out(addr));
+	wide_mux2_1  UncondBr_mux (.s0(UncondBr), .A(SE_condAddr19), .B(SE_BrAddr26), .OUT(addr));
 	
 	// add PC and branch value 
 	// PC + SE(Addr) << 2
-	logic [63:0] sum_PCandImm, carry_PCandImm;
-	add64 PCandImm (.A(addr), .B(instr_addr), .subtract_signal(1'b0), .sum(sum_PCandImm), .carry_out(carry_PCandImm));
+	logic [63:0] sum_PCandImm;
+	logic carry_PCandImm;
+	// TODO - ADD64 NOT IN THIS PROJECT
+	add_width PCandImm (.A(addr), .B(instr_addr), .cIn(1'b0), .s(sum_PCandImm), .cOut(carry_PCandImm));
 	
 	// 2:1 Mux to choose between PC+4 and PC+addr
 	logic [63:0] nextPC;
-	mux2_1 BrTaken_mux (.s0(BrTaken), .a(PC4), .b(sum_PCandImm), .out(nextPC));
+	wide_mux2_1  BrTaken_mux (.s0(BrTaken), .A(PC4), .B(sum_PCandImm), .OUT(nextPC));
 	
+	// implement PC
+	register PC (.Din(nextPC), .Dout(instr_addr), .enable(1'b1), .clk, .reset);
+	
+	// instruction mem
+	// TODO NEVER UPDATING INSTR_ADDR
 	instructmem instr (.address(instr_addr), .instruction, .clk);
 	
+	
 endmodule
-// EDITS MADE:
-// - removed instruction address as an input
-// - assumed that instruction address is not something that is used anywhese else,
-//   can keep it within the module
-// - added initialization of instructmem here. makes sense here and not in datapath
-// - changed instruction from input logic to output logic. driven by instructmem
+
+
+`timescale 1ps/1ps
+module program_counter_testbench ();
+	logic [63:0] instr_addr; // address of instruction
+	logic [31:0] instruction;
+	logic UncondBr, BrTaken, clk, reset; // control inputs
+
+	program_counter dut (.*);
+	
+	parameter CLOCK_PERIOD=5000;
+	initial begin
+		clk <= 0;
+		forever #(CLOCK_PERIOD/2) clk <= ~clk; // Forever toggle the clock
+	end
+
+	initial begin
+		reset <= 1'b1; UncondBr <= 1'b0; BrTaken <= 1'b0;  @(posedge clk);
+		reset <= 1'b0; 									repeat(1)@(posedge clk);
+//		while
+//		if (instruction == 32'b000101_00000000000000000000001100) begin
+//		UncondBr <= 1'b0; BrTaken <= 1'b0;  @(posedge clk);// B FORWARD_B          // 1st taken branch (+12)
+//		// test with branches next
+//		reset <= 1'b1;					
+//		reset <= 1'b0;
+	// end
+		$stop;
+	end
+endmodule
+
